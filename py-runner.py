c07488f86c4ec0752be44dec68f56e3289f889b9
Owen Christie <owen@Owens-MacBook-Air.local>
2020-06-25 01:27:24 -0400

    using unix sockets, better example

--- py-runner.py
+++ py-runner.py
@@ -1,3 +1,5 @@
+import os
+import traceback
 import subprocess
 import concurrent.futures
 import rx
@@ -5,42 +7,42 @@ from rx import operators as ops
 import tempfile
 import socket
 
-HOST = '127.0.0.1'
-PORT = 65432
-
-# TODO: add message length to the begenning of message so we can specify how many bytes to read
-# TODO: use unix sockets instead of tcp/ip
 # TODO: add some way to dynamically spawn new workers (with new socket server) so we can handle multiple connections at once
 
-def get_input():
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.bind((HOST, PORT))
+def get_input(socket_loc: str):
+    with socket.socket(socket.AF_UNIX, socket.SOCK_STREAM) as s:
+        if os.path.exists(socket_loc):
+            os.remove(socket_loc)
+        s.bind(socket_loc)
         s.listen()
-        print(f'Listening on {(HOST, PORT)}')
+        print(f'Listening to socket located at {socket_loc}')
         while True:
             conn, addr = s.accept()
             lines = []
             with conn:
                 print('Connected by', addr)
-                while True:
-                    line = conn.recv(1024)
-                    decoded_line = line.decode()
-                    if decoded_line is not None and decoded_line != '\q':
-                        lines.append(decoded_line)
-                    else:
-                        break
+                message_len_raw = conn.recv(128)
+                message_len_decoded = message_len_raw.decode()
+                message_len = int(message_len_decoded.replace('-', '')) 
+                    
+                message = conn.recv(message_len)
+                code = message.decode()
 
-                    if '\q' in decoded_line:
-                        break
-
-                in_ = ''.join(lines).replace('\q', '') 
                 print('Running:')
-                print(in_)
+                print(code)
+
                 with tempfile.NamedTemporaryFile() as f:
-                    f.write(in_.encode())
+                    f.write(code.encode())
                     f.seek(0)
-                    output = subprocess.check_output(f'python3 {f.name}', shell=True, stderr=subprocess.STDOUT)  
-                    conn.sendall(output)
+                    try:
+                        output = subprocess.check_output(f'python3 {f.name}', shell=True, stderr=subprocess.STDOUT)  
+                    except subprocess.CalledProcessError as e:
+                        output = traceback.format_exc().encode()
+                        print(output)
+                    message_len = len(output)
+                    header = str(message_len)
+                    header = header + '-' * (128 - len(header))  # pad first 128 bytes
+                    conn.sendall(header.encode() + output)
                     yield output
 
 
@@ -53,9 +55,10 @@ def get_output(output):
 def return_item(item):
     return item
 
+socket_loc = '/tmp/py_runner.sock'
 
 with concurrent.futures.ThreadPoolExecutor(5) as executor: 
-    rx.from_(get_input()).pipe(
+    rx.from_(get_input(socket_loc)).pipe(
         ops.flat_map(lambda item: executor.submit(return_item, item)),
     ).subscribe(get_output)
     
--- test_send.py
+++ test_send.py
@@ -1,24 +1,37 @@
 import socket
 
-HOST = '127.0.0.1'  # The server's hostname or IP address
-PORT = 65432        # The port used by the server
+socket_loc = '/tmp/py_runner.sock'
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((HOST, PORT))
-    code = b'''def a():
-    print(42)
-a()
-
+with socket.socket(socket.AF_UNIX, socket.SOCK_STREAM) as s:
+    s.connect(socket_loc)
+    code = b'''
 import numpy as np
-zeros = np.zeros(100)
-print(zeros)
+import random
 
-print([i for i in range(1000)])
-\q'''
-    s.sendall(code)
-    data = s.recv(1024)
-    out = data.decode()
+random_vec = np.random.rand(3)
+random_vec_2 = np.random.rand(3)
+
+random_coefficient = random.randint(-10, 10)
+random_coefficient_2 = random.randint(-10, 10)
+
+question = f"What is {random_coefficient} * {random_vec.tolist()} + {random_coefficient_2} * {random_vec_2.tolist()}?"
+answer = (random_coefficient * random_vec) + (random_coefficient_2 * random_vec_2)
+
+print(question)
+print(answer.tolist())
+
+'''
+    message_len = len(code)
+    header = str(message_len)
+    header = header + '-' * (128 - len(header))  # pad first 128 bytes
+    s.sendall(header.encode() + code)
+    
+    message_len_raw = s.recv(128)
+    message_len_decoded = message_len_raw.decode()
+    message_len = int(message_len_decoded.replace('-', '')) 
+    message = s.recv(message_len)
+    output = message.decode()
 
 print('Output:')
-print(out)
+print(output)
 
