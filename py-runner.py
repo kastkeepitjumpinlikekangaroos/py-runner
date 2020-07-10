bf295e5624b11e6b095f833adb44b6653c154ab8
Owen Christie <owen@Owens-MacBook-Air.local>
2020-07-09 22:07:55 -0400

    refactor

--- py-runner.py
+++ py-runner.py
@@ -3,14 +3,29 @@ import logging
 import traceback
 import subprocess
 import tempfile
+import os
+from functools import partial
+from concurrent.futures import ThreadPoolExecutor
 
 SOCKET_LOC = '/tmp/.py-runner/py_runner.sock'
 HEADER_LEN = 128
 
 
-async def handle_connection(reader, writer):
+async def main():
+    logging.info(f'Serving on {SOCKET_LOC}')
+    loop = asyncio.get_event_loop()
+    executor = ThreadPoolExecutor(max_workers=os.cpu_count())
+    __handle_connection = partial(_handle_connection, executor=executor, loop=loop)
+    server = await asyncio.start_unix_server(__handle_connection, SOCKET_LOC)
+
+    async with server:
+        await server.serve_forever()
+
+
+async def _handle_connection(reader, writer, loop, executor):
     addr = writer.get_extra_info('peername')
     logging.info(f'Handling request for {addr} on {SOCKET_LOC}')
+
     message_len_raw = await reader.read(HEADER_LEN)
     message_len_decoded = message_len_raw.decode()
     message_len = int(message_len_decoded.replace('-', ''))
@@ -19,6 +34,20 @@ async def handle_connection(reader, writer):
     code = message.decode()
     logging.info(f'Running:\n\n{code}')
 
+
+    output = await loop.run_in_executor(executor, _exec_code, code)
+
+    message_len = len(output)
+    header = str(message_len)
+    # encode message length in first HEADER_LEN bytes
+    header = header + '-' * (HEADER_LEN - len(header))
+    writer.write(header.encode() + output)
+    await writer.drain()
+    logging.info(f'Close the connection for {addr}')
+    writer.close()
+
+
+def _exec_code(code: str) -> bytes:
     with tempfile.NamedTemporaryFile() as f:
         f.write(code.encode())
         f.seek(0)
@@ -27,32 +56,16 @@ async def handle_connection(reader, writer):
         except subprocess.CalledProcessError as e:
             output = _log_error(e).encode()
     _log_output(output)
-    message_len = len(output)
-    header = str(message_len)
-    # encode message length in first HEADER_LEN bytes
-    header = header + '-' * (HEADER_LEN - len(header))
-    writer.write(header.encode() + output)
-    await writer.drain()
-    logging.info("Close the connection for {addr}")
-    writer.close()
+    return output
 
 
-async def main():
-    logging.info(f'Serving on {SOCKET_LOC}')
-    server = await asyncio.start_unix_server(
-        handle_connection, SOCKET_LOC)
-
-    async with server:
-        await server.serve_forever()
-
-
-def _log_output(output):
+def _log_output(output: bytes) -> str:
     decoded_out = output.decode()
     logging.info(decoded_out)
     return decoded_out
 
 
-def _log_error(e):
+def _log_error(e) -> str:
     err = traceback.format_exc()
     err = f'Error: {e}\n\n{err}'
     logging.warning(err)
@@ -62,3 +75,4 @@ def _log_error(e):
 if __name__ == '__main__':
     logging.basicConfig(format='%(asctime)s - %(message)s', level=logging.INFO)
     asyncio.run(main())
+
